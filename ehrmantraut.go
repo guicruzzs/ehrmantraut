f451f196d9a88e1a3c0be1d62d839fab0d4c984a
Guilherme Cruz <guicruz.zs@gmail.com>
2017-09-20 00:35:26 -0300

    Add chanels to receive and consume messages

--- ehrmantraut.go
+++ ehrmantraut.go
@@ -6,11 +6,23 @@ import(
 	"os"
 )
 
-func main(){
-	for true {
-		reader := bufio.NewReader(os.Stdin)
-		fmt.Print("New data arriving:")
+func receiveMosquittoData(mosquittoStreaming chan string){
+	reader := bufio.NewReader(os.Stdin)
+	for {
 		text, _ := reader.ReadString('\n')
-		fmt.Println(text)
+		mosquittoStreaming <- text
 	}
+}
+
+func storeData(mosquittoStreaming chan string){
+	for {
+		fmt.Print("Stored: ", <- mosquittoStreaming)
+	}
+}
+
+func main(){
+	mosquittoStreaming := make(chan string)
+
+	go storeData(mosquittoStreaming)
+	receiveMosquittoData(mosquittoStreaming)
 }
