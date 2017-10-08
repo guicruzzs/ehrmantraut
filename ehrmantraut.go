cec19b471369ed051a13a088d91f9df8d26a72cd
Guilherme Cruz <guicruz.zs@gmail.com>
2017-10-07 22:11:06 -0300

    Parse car data

--- ehrmantraut.go
+++ ehrmantraut.go
@@ -4,24 +4,45 @@ import(
 	"fmt"
 	"bufio"
 	"os"
+	"encoding/json"
 )
+// TODO: Data handling and DB should be in their places, not with the main program
+type CarData struct {
+    Fields map[string]interface{} `json:"fields"`
+    Tags map[string]interface{} `json:"tags"`
+    Time int64 `json:"time"`
+}
 
-func receiveMosquittoData(mosquittoStreaming chan string){
+// TODO: In error case: it should properly be logged and the error should be returned
+func parseJSON(jsonData []byte) CarData {
+	var data CarData
+	err := json.Unmarshal(jsonData, &data)
+
+	if err != nil {
+		fmt.Println("Parse error")
+	}
+	return data
+}
+
+// TODO: In parse error, it shouldn't store data
+func storeData(mosquittoStreaming chan []byte){
+	for {
+		jsonData := <- mosquittoStreaming
+		carData := parseJSON(jsonData)
+		fmt.Printf("Stored: %s", carData)
+	}
+}
+
+func receiveMosquittoData(mosquittoStreaming chan []byte){
 	reader := bufio.NewReader(os.Stdin)
 	for {
-		text, _ := reader.ReadString('\n')
+		text, _ := reader.ReadBytes('\n')
 		mosquittoStreaming <- text
 	}
 }
 
-func storeData(mosquittoStreaming chan string){
-	for {
-		fmt.Print("Stored: ", <- mosquittoStreaming)
-	}
-}
-
 func main(){
-	mosquittoStreaming := make(chan string)
+	mosquittoStreaming := make(chan []byte)
 
 	go storeData(mosquittoStreaming)
 	receiveMosquittoData(mosquittoStreaming)
